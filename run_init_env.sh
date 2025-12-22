ff3bd636b724c6eb1a295ccae82d8d370d50033d
AnhQuangDuong <quanganh2102004@gmail.com>
2025-12-22 08:43:02 +0000

    add clean mongodb

--- run_init_env.sh
+++ run_init_env.sh
@@ -3,6 +3,9 @@
 # Navigate to the project directory
 source venv/bin/activate
 
+# Start minikube
+minikube start
+
 # Start Docker Compose in detached mode
 docker compose up -d
 
@@ -13,4 +16,19 @@ start-dfs.sh
 sleep 5
 
 # Execute admin.py
-python admin.py
+python3 admin.py
+
+# reset speed layers collection
+docker exec -it mongodb mongosh tft_db --eval "db.players_speed.deleteMany({})"
+
+# reset comp collection
+docker exec -it mongodb mongosh tft_db --eval "db.compositions.deleteMany({})"
+
+# reset item collection
+docker exec -it mongodb mongosh tft_db --eval "db.items.deleteMany({})"
+
+# reset trait collection
+docker exec -it mongodb mongosh tft_db --eval "db.traits.deleteMany({})"
+
+# reset unit collection
+docker exec -it mongodb mongosh tft_db --eval "db.units.deleteMany({})"
